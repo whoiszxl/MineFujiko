f65a9e1d5de98dc5d8055c0e572459c700390faa
zhouxiaolong <whoiszxl@gmail.com>
2018-02-17 22:45:06 +0800

    自己手动配置autowired http类

--- code/house/src/test/java/com/whoiszxl/house/HouseApplicationTests.java
+++ code/house/src/test/java/com/whoiszxl/house/HouseApplicationTests.java
@@ -1,7 +1,15 @@
 package com.whoiszxl.house;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +17,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class HouseApplicationTests {
 
+	@Autowired
+	private HttpClient httpClient;
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	
+	@Test
+	public void testHttpClient() throws ClientProtocolException, IOException {
+		HttpEntity a = httpClient.execute(new HttpGet("http://whoiszxl.com")).getEntity();
+		
+		System.out.println(EntityUtils.toString(a));
 	}
 
 }
