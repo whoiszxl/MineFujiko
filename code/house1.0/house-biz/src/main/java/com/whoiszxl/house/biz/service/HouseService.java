bb1b8c28c0221232606f73aa3ed5fa5bd9b43a9a
zhouxiaolong <whoiszxl@gmail.com>
2018-02-24 18:37:05 +0800

    房屋列表的实体类和分页实体类编写以及controller的一部分

---
+++ code/house1.0/house-biz/src/main/java/com/whoiszxl/house/biz/service/HouseService.java
@@ -0,0 +1,8 @@
+package com.whoiszxl.house.biz.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HouseService {
+
+}
---
+++ code/house1.0/house-web/src/main/java/com/whoiszxl/house/web/controller/HouseController.java
@@ -0,0 +1,38 @@
+package com.whoiszxl.house.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.whoiszxl.house.biz.service.HouseService;
+import com.whoiszxl.house.common.model.House;
+
+/**
+ * 房屋控制器
+ * @author whoiszxl
+ *
+ */
+@Controller
+public class HouseController {
+
+	@Autowired
+	private HouseService houseService;
+	
+	
+	/**
+	 * 1.实现分页
+	 * 2.支持小区搜索、类型搜索
+	 * 3.支持排序
+	 * 4.支持展示图片、价格、标题、地址等信息
+	 * @return
+	 */
+	@RequestMapping("/house/list")
+	public String houseList(Integer pageSize,Integer pageNum,House query,ModelMap modelMap) {
+		
+		
+		return null;
+	}
+	
+	
+}
