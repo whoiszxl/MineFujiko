8bfb964b065b8a1261623a76b8ab38d85ab03d8d
zhouxiaolong <whoiszxl@gmail.com>
2018-02-28 11:45:09 +0800

    spring admin监控

---
+++ code/spring-admin/src/main/java/com/whoiszxl/admin/SpringAdminApplication.java
@@ -0,0 +1,15 @@
+package com.whoiszxl.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminApplication.class, args);
+	}
+}
