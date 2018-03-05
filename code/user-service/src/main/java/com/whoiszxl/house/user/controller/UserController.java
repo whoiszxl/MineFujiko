7c786c658f7f40571e953a4829b59c707acc771a
zhouxiaolong <whoiszxl@gmail.com>
2018-03-05 12:02:27 +0800

    eureka接入ribbon负载均衡

--- code/user-service/src/main/java/com/whoiszxl/house/user/controller/UserController.java
+++ code/user-service/src/main/java/com/whoiszxl/house/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.whoiszxl.house.user.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,12 @@ public class UserController {
 
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
+	@Value("${server.port}")
+	private Integer port;
+	
 	@RequestMapping("getusername")
 	public RestResponse<String> getUsername(Long id) {
 		logger.info("request getUsername method");
-		return RestResponse.success("test getUsername");
+		return RestResponse.success("test getUsername port:" + port);
 	}
 }
